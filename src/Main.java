19d0d41ec8463d817c2184363658d9ba27c6e0c8
inix84 <inix84@mail.ru>
2022-10-16 10:53:56 +0500

    Задание 2 (в требованиях нет цикла, выполнила путем копирования отдельного массива, его сортировкой, и вывода первого и последнего значения)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 public class Main {
-    int[] arr = generateRandomArray();
+   // int[] arr = generateRandomArray();
 
     public static int[] generateRandomArray() {
                 java.util.Random random = new java.util.Random();
@@ -12,9 +12,20 @@ public class Main {
             }
     public static void main(String[] args) {
 int sum=0;
-        for (int element: generateRandomArray()) {
+//int sum1=0;
+        int[] arr = generateRandomArray();
+        for (int element: arr) {
             sum+=element;
         }
         System.out.println( "Сумма трат за месяц составила "+sum+ " рублей");
+
+        //for (int index=0;index< arr.length;index++){
+        //    sum1+=arr[index];
+        //}
+       //System.out.println( "Сумма трат за месяц составила "+sum1+ " рублей");
+
+        int [] arr2=Arrays.copyOf(arr,arr.length);
+        Arrays.sort(arr2);
+        System.out.println( "минимальные траты "+arr2[0]+ " максимальные траты  "+arr2[arr2.length-1]);
     }
 }
